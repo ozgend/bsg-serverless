16e679c96e377c293195052603257d02687e4439
ozgend <revolter00@gmail.com>
2017-06-08 02:06:24 +0300

    fixed document toModel conversion

--- handler.js
+++ handler.js
@@ -13,13 +13,12 @@ module.exports.find = (event, context, callback) => {
 
   db.once('open', () => {
     FuckModel
-      .find({ Slug: slug })
+      .findOne({ Slug: slug })
       .then((item) => {
-        delete item.track;
-        delete item._id;
-        callback(null, { statusCode: 200, body: JSON.stringify({ success: true, fuckoff: item }) });
+        callback(null, { statusCode: 200, body: JSON.stringify({ success: true, fuckoff: item.toResponseModel() }) });
       })
       .catch((err) => {
+        console.log(err);
         callback(err, { statusCode: 200, body: JSON.stringify({ success: false }) });
       })
       .finally(() => {
@@ -28,3 +27,21 @@ module.exports.find = (event, context, callback) => {
   });
 };
 
+function convertToResponseModel(dbo) {
+  return { To: dbo.To, From: dbo.From, Message: dbo.Message, Time: dbo.Time, Timestamp: dbo.Timestamp, views: dbo.views };
+}
+
+FuckModel.prototype.toResponseModel = function () {
+  return { To: this.To, From: this.From, Message: this.Message, Time: this.Time, Timestamp: this.Timestamp, views: this.views };
+}
+
+/*
+String.prototype.sanitize = function () {
+  return validator.stripLow(this);
+}
+
+Date.utcNow = function () {
+  var now = new Date();
+  return Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds(), now.getUTCMilliseconds());
+}
+*/
